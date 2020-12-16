475c419bbb2841a577fd16fb62b6ba07be11c890
Driijko <driijko@gmail.com>
2020-12-16 18:18:58 -0500

    Complete uniform response layer container sizing

--- src/components/UniformResponse/UniformLayerContainer.js
+++ src/components/UniformResponse/UniformLayerContainer.js
@@ -1,8 +1,12 @@
-export default function UniformLayerContainer(props) {
-    return (
-        <div>
-            <p>Layer Container Width: {String(props.width)}</p>
-            <p>Layer Container Height: {String(props.height)}</p>
-        </div>
-    )
-}
+import styled, {css} from "styled-components";
+
+const UniformLayerContainer = styled.div`${props => css`
+    box-sizing: border-box;
+    width: ${props.width}px;
+    height: ${props.height}px;
+
+    background-color: black;
+    color: white;
+`}`;
+
+export default UniformLayerContainer;
--- src/components/UniformResponse/UniformResponse.js
+++ src/components/UniformResponse/UniformResponse.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+import styled, {css} from "styled-components";
 
 import UniformLayerContainer from "./UniformLayerContainer";
 
@@ -7,47 +8,77 @@ export default function UniformResponse(props) {
     // Fundamental Aspect Ratio ///////////////////////////////////////////////
     const ratioWidth = 2;
     const ratioHeight = 3;
+    const ratio = ratioWidth / ratioHeight;
 
-    // STATE ////////////////////////////////////////////////////
+    // STATE ///////////////////////////////////////////////////////////////////////
+
+    // Size -------------------------------------------------------------------
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    // Layer Containers ------------------------------------------------------
     const [layerContainerWidth, setLayerContainerWidth] = useState(null);
     const [layerContainerHeight, setLayerContainerHeight] = useState(null);
 
     function calculateLayerContainerSize() {
-        const ratio = ratioWidth / ratioHeight;
 
         if (ratio <= 1) {
-            if (window.innerWidth <= window.innerHeight) {
-                setLayerContainerWidth(window.innerWidth);
-                setLayerContainerHeight(window.innerWidth * (ratioHeight/ratioWidth));
-            }
-            else {
-                setLayerContainerHeight(window.innerHeight);
-                setLayerContainerWidth(window.innerHeight * (ratioWidth/ratioHeight));
-            }
+            const width = Math.min(
+                window.innerWidth,
+                Math.round(window.innerHeight * (ratioWidth/ratioHeight)) 
+            );
+            setLayerContainerWidth(width);
+            setLayerContainerHeight(Math.round(width * (ratioHeight/ratioWidth)));
+        }
+        else {
+            const height = Math.min(
+                window.innerHeight,
+                Math.round(window.innerWidth * (ratioHeight/ratioWidth))
+            );
+            setLayerContainerHeight(height);
+            setLayerContainerWidth(Math.round(height * (ratioWidth/ratioHeight)));
         }
     }
 
     // This makes sure we calculate the layer container size on first render.
     useEffect(()=> {
         calculateLayerContainerSize();
-    },[])
+    },[]);
 
-    // EVENT HANDLERS ////////////////////////////////////////////
+    // DUAL CONTAINER //////////////////////////////////////////////////////////////
+    function setupDualContainer() {
+        if (ratio <= 1) {
+            if (window.innerWidth > layerContainerWidth * 2) {
+                
+            }
+        }
+    }
+
+    // EVENT HANDLERS ////////////////////////////////////////////////////////////
     window.addEventListener("resize", () => {
         calculateLayerContainerSize();
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     });
 
-    // RENDER ///////////////////////////////////////////////////
+    // STYLES /////////////////////////////////////////////////////////////////////
+    const UniformResponseDiv = styled.div`${props=> css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: ${width}px;
+        height: ${height}px;
+    `}`;
+
+    // RENDER //////////////////////////////////////////////////////////////////
     return (
-        <div>
-            <p>Window Width: {String(window.innerWidth)}</p>
-            <p>Window Height: {String(window.innerHeight)}</p>
+        <UniformResponseDiv>
             <UniformLayerContainer 
                 width={layerContainerWidth}
                 height={layerContainerHeight}
             >
             </UniformLayerContainer>
-        </div>
-    )
-    
+        </UniformResponseDiv>
+    )   
 }
